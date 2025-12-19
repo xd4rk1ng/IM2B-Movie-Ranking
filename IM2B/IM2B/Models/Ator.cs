d0c90f0123c3cd0415654d732d0f4ea011ca8ff8
Sergio Sant'Anna <123362811+SergioPastel@users.noreply.github.com>
2025-12-19 14:43:03 +0000

    Squashed commit of the following:
    commit afe7aac0bc16e6dad3fbced206a6af7970d4d1cb
    Author: Sergio Sant'Anna <123362811+SergioPastel@users.noreply.github.com>
    Date:   Fri Dec 19 14:33:30 2025 +0000
    
        Created models
    
        Created some models and ViewModels.
        To be honest I don't know what I'm supposed to do.

---
+++ IM2B/IM2B/Models/Ator.cs
@@ -0,0 +1,11 @@
+﻿namespace IM2B.Models
+{
+    public class Ator
+    {
+        public string Nome { get; set; }
+        public DateOnly DataNasc { get; set; }
+        public DateOnly? DataObito { get; set; }
+        public string Biografia { get; set; }
+        public List<Filme> Filmes { get; set; }
+    }
+}
