1aa9c0174fb0340e072699e74224d4f769149766
diegomayrinck <157934919+diegomayrinck@users.noreply.github.com>
2025-12-26 21:50:57 +0000

    Adicionando Autenticação do Curador
    Adicionando autenticação do Curador

---
+++ IM2B/IM2B/Models/Papel.cs
@@ -0,0 +1,13 @@
+﻿namespace IM2B.Models
+{
+    public class Papel
+    {
+        public int Id { get; set; }
+        public int FilmeId { get; set; }
+        public Filme Filme { get; set; }
+        public int AtorId { get; set; }
+        public Ator Ator { get; set; }
+        public string Personagem { get; set; }
+        public bool Principal { get; set; }
+    }
+}
