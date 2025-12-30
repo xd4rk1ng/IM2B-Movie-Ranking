adf0b547810e93db2a3585fd53abc6d3b239cd02
Sergio Sant'Anna <123362811+SergioPastel@users.noreply.github.com>
2025-12-30 20:47:07 +0000

    Squashed commit of the following:
    commit 1aa9c0174fb0340e072699e74224d4f769149766
    Author: diegomayrinck <157934919+diegomayrinck@users.noreply.github.com>
    Date:   Fri Dec 26 21:50:57 2025 +0000
    
        Adicionando Autenticação do Curador
    
        Adicionando autenticação do Curador
    
    commit 91f4e36ecd25245b95bad3a983da6e2814e0ead3
    Author: diegomayrinck <157934919+diegomayrinck@users.noreply.github.com>
    Date:   Fri Dec 26 21:30:42 2025 +0000
    
        Criação do AtorController.cs e do FilmeController.cs
    
        Criação do AtorController.cs e do FilmeController.cs. É preciso testar de acordo com os Models e as Views.
    
    commit 9fb4f225dae08700011633bfd9a497499482643e
    Author: talits-prog <talita.calaca@gmail.com>
    Date:   Mon Dec 22 12:34:10 2025 +0000
    
        index_filmes
    
    commit 9d8d1b409be5bf1d940a15e30f6a72e329b36dc5
    Author: talits-prog <talita.calaca@gmail.com>
    Date:   Mon Dec 22 12:18:24 2025 +0000
    
        _Layout
    
    commit a78b336102ab8daef6266ac208741a31b444b713
    Author: talits-prog <talita.calaca@gmail.com>
    Date:   Mon Dec 22 11:53:20 2025 +0000
    
        index

---
+++ IM2B/IM2B/Controllers/FilmeController.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using IM2B.Models;
+using IM2B.ViewModels;
+
+namespace IM2B.Controllers
+{
+    public class FilmeController : Controller
+    {
+        // TODO: Adicionar DbContext quando configurar o banco de dados
+        // private readonly ApplicationDbContext _context;
+
+        // Index - Listar todos os filmes
+        public IActionResult Index()
+        {
+            // TODO: var filmes = _context.Filmes.ToList();
+            var filmes = new List<Filme>(); // Placeholder
+            return View(filmes);
+        }
+
+        // Details - Ver detalhes de um filme específico
+        public IActionResult Details(int id)
+        {
+            // TODO: var filme = _context.Filmes.Include(f => f.Atores).FirstOrDefault(f => f.Id == id);
+            // if (filme == null) return NotFound();
+
+            var filme = new Filme(); // Placeholder
+            return View(filme);
+        }
+
+        // Create GET - Formulário para criar novo filme
+        [Authorize(Roles = "Curador")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Create POST - Processar criação do filme
+        [Authorize(Roles = "Curador")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Filme filme)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: _context.Filmes.Add(filme);
+                // TODO: _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(filme);
+        }
+
+        // Edit GET - Formulário para editar filme
+        [Authorize(Roles = "Curador")]
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            // TODO: var filme = _context.Filmes.Find(id);
+            // if (filme == null) return NotFound();
+
+            var filme = new Filme(); // Placeholder
+            return View(filme);
+        }
+
+        // Edit POST - Processar edição do filme
+        [Authorize(Roles = "Curador")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Filme filme)
+        {
+            if (id != filme.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                // TODO: _context.Update(filme);
+                // TODO: _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(filme);
+        }
+
+        // Delete GET - Confirmar exclusão
+        [Authorize(Roles = "Curador")]
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            // TODO: var filme = _context.Filmes.Find(id);
+            // if (filme == null) return NotFound();
+
+            var filme = new Filme(); // Placeholder
+            return View(filme);
+        }
+
+        // Delete POST - Processar exclusão
+        [Authorize(Roles = "Curador")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            // TODO: var filme = _context.Filmes.Find(id);
+            // TODO: _context.Filmes.Remove(filme);
+            // TODO: _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // AtribuirPapel GET - Formulário para atribuir ator a filme
+        [Authorize(Roles = "Curador")]
+        [HttpGet]
+        public IActionResult AtribuirPapel(int filmeId)
+        {
+            // TODO: var filme = _context.Filmes.Find(filmeId);
+            // if (filme == null) return NotFound();
+
+            // TODO: ViewBag.Atores = new SelectList(_context.Atores, "Id", "Nome");
+
+            var viewModel = new AtribuirPapelViewModel
+            {
+                FilmeId = filmeId
+            };
+
+            return View(viewModel);
+        }
+
+        // AtribuirPapel POST - Processar atribuição de papel
+        [Authorize(Roles = "Curador")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AtribuirPapel(AtribuirPapelViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                // TODO: ViewBag.Atores = new SelectList(_context.Atores, "Id", "Nome");
+                return View(viewModel);
+            }
+
+            // Validação: Impedir atribuição duplicada
+            // TODO: var filme = _context.Filmes.Include(f => f.Atores)
+            //           .FirstOrDefault(f => f.Id == viewModel.FilmeId);
+
+            // TODO: var atorJaAssociado = filme.Atores.Any(a => a.Id == viewModel.AtorId);
+
+            // if (atorJaAssociado)
+            // {
+            //     ModelState.AddModelError("", "Este ator já está associado a este filme.");
+            //     ViewBag.Atores = new SelectList(_context.Atores, "Id", "Nome");
+            //     return View(viewModel);
+            // }
+
+            // TODO: var ator = _context.Atores.Find(viewModel.AtorId);
+            // TODO: filme.Atores.Add(ator);
+            // TODO: _context.SaveChanges();
+
+            return RedirectToAction(nameof(Details), new { id = viewModel.FilmeId });
+        }
+
+        // RemoverAtor - Remover ator de um filme
+        [Authorize(Roles = "Curador")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult RemoverAtor(int filmeId, int atorId)
+        {
+            // TODO: var filme = _context.Filmes.Include(f => f.Atores)
+            //           .FirstOrDefault(f => f.Id == filmeId);
+
+            // if (filme == null) return NotFound();
+
+            // TODO: var ator = filme.Atores.FirstOrDefault(a => a.Id == atorId);
+            // if (ator != null)
+            // {
+            //     filme.Atores.Remove(ator);
+            //     _context.SaveChanges();
+            // }
+
+            return RedirectToAction(nameof(Details), new { id = filmeId });
+        }
+    }
+}
---
+++ IM2B/IM2B/Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using IM2B.Models;
+
+namespace IM2B.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+        public DbSet<Ator> Atores { get; set; }
+        public DbSet<Papel> Papeis { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Configurar relação M:N entre Filme e Ator através de Papel
+            builder.Entity<Papel>()
+                .HasOne(p => p.Filme)
+                .WithMany()
+                .HasForeignKey(p => p.FilmeId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Papel>()
+                .HasOne(p => p.Ator)
+                .WithMany()
+                .HasForeignKey(p => p.AtorId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Impedir duplicação: mesmo ator não pode ter o mesmo papel duas vezes no mesmo filme
+            builder.Entity<Papel>()
+                .HasIndex(p => new { p.FilmeId, p.AtorId, p.Personagem })
+                .IsUnique();
+        }
+    }
+}
