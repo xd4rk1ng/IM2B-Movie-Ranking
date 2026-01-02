14812085d7a64b01b2aca55ccf55ab42a6160dbf
D4rKing <rui.santos.maia@gmail.com>
2026-01-02 15:14:56 +0000

    Added mapping between Entities and Models

---
+++ IM2B/IM2B/Mapping/AtorMapper.cs
@@ -0,0 +1,36 @@
+﻿using IM2B.Models;
+using context.Entities;
+
+namespace IM2B.Mapping
+{
+    public static class AtorMapper
+    {
+        public static Ator ToModel(this AtorEntity entity)
+        {
+            // Usa-se o construtor da classe para permitir validacoes e logica de negocio
+            return new
+            (
+                entity.Id,
+                entity.Nome,
+                entity.DataNasc,
+                entity.DataObito,
+                entity.Biografia,
+                entity.Filmes.Select(e => e.ToModel()).ToList()
+            );
+        }
+
+        public static AtorEntity ToEntity(this Ator model)
+        {
+            // Usa-se este inicializador basico porque a entidade nao deve possuir metodos, ou seja, nenhum construtor a usar
+            return new()
+            {
+                Id = model.Id,
+                Nome = model.Nome,
+                DataNasc = model.DataNasc,
+                DataObito = model.DataObito,
+                Biografia = model.Biografia,
+                Filmes = model.Filmes.Select(e => e.ToEntity()).ToList(),
+            };
+        }
+    }
+}
---
+++ IM2B/IM2B/Mapping/PapelMapper.cs
@@ -0,0 +1,38 @@
+﻿using IM2B.Models;
+using context.Entities;
+
+namespace IM2B.Mapping
+{
+    public static class PapelMapper
+    {
+        public static Papel ToModel(this PapelEntity entity)
+        {
+            // Usa-se o construtor da classe para permitir validacoes e logica de negocio
+            return new
+            (
+                entity.Id,
+                entity.FilmeId,
+                entity.Filme.ToModel(),
+                entity.AtorId,
+                entity.Ator.ToModel(),
+                entity.Personagem,
+                entity.Principal
+            );
+        }
+
+        public static PapelEntity ToEntity(this Papel model)
+        {
+            // Usa-se este inicializador basico porque a entidade nao deve possuir metodos, ou seja, nenhum construtor a usar
+            return new()
+            {
+                Id = model.Id,
+                FilmeId = model.FilmeId,
+                Filme = model.Filme.ToEntity(),
+                AtorId = model.AtorId,
+                Ator = model.Ator.ToEntity(),
+                Personagem = model.Personagem,
+                Principal = model.Principal
+            };
+        }
+    }
+}
