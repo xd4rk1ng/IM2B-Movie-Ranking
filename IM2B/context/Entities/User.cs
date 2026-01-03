9d1390603a2bfd358535b0c84161b10d020a8f46
talits-prog <talita.calaca@gmail.com>
2026-01-03 02:05:22 +0000

    add papel e user

---
+++ IM2B/context/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IM2B.Models
+
+{
+    public class User : IdentityUser
+    {
+        public string NomeCompleto { get; set; }
+
+        public string NIF { get; set; }
+
+        public bool IsCurador { get; set; }
+    }
+}
